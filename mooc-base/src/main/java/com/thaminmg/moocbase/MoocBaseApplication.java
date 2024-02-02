aee1a5b3a5caed4325ed427815a05931b58b6ef8
Thiha MM <thaminmg@gmail.com>
2024-02-01 21:07:19 -0800

    Parent and Base directory added.

---
+++ mooc-base/src/main/java/com/thaminmg/moocbase/MoocBaseApplication.java
@@ -0,0 +1,13 @@
+package com.thaminmg.moocbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoocBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoocBaseApplication.class, args);
+	}
+
+}
---
+++ mooc-base/src/test/java/com/thaminmg/moocbase/MoocBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thaminmg.moocbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoocBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
